8a81d3393f927bbd80d512fa780944d2f4463748
cs-mac <halfmoonvic@gmail.com>
2023-01-11 13:24:26 +0800

    chore: normalize code

--- lua/time.lua
+++ lua/time.lua
@@ -8,6 +8,13 @@ translator 的功能是将分好段的输入串翻译为一系列候选项。
  - seg: 包含 `start` 和 `_end` 两个属性，分别表示当前串在输入框中的起始和结束位置
  - env: 可选参数，表示 translator 所处的环境（本例没有体现）
 
+候选项的构造函数是 `Candidate`，它有五个参数：
+ - type: 字符串，表示候选项的类型
+ - start: 候选项对应的输入串的起始位置
+ - _end:  候选项对应的输入串的结束位置
+ - text:  候选项的文本
+ - comment: 候选项的注释
+
 translator 的输出是若干候选项。
 与通常的函数使用 `return` 返回不同，translator 要求您使用 `yield` 产生候选项。
 
@@ -17,34 +24,26 @@ translator 的输出是若干候选项。
 --]]
 
 local function translator(input, seg)
-    -- 如果输入串为 `date` 则翻译
-    if (input == "date") then
-        --[[ 用 `yield` 产生一个候选项
-           候选项的构造函数是 `Candidate`，它有五个参数：
-            - type: 字符串，表示候选项的类型
-            - start: 候选项对应的输入串的起始位置
-            - _end:  候选项对应的输入串的结束位置
-            - text:  候选项的文本
-            - comment: 候选项的注释
-       --]]
-        yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), "日期"))
-        --[[ 用 `yield` 再产生一个候选项
-           最终的效果是输入法候选框中出现两个格式不同的当前日期的候选项。
-      --]]
-        yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), "日期"))
-    end
+  local weakTab = { '日', '一', '二', '三', '四', '五', '六' }
 
-    if (input == "time") then
-        yield(Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), " 时间"))
-    end
+  if (input == "time") then
+    local time = os.date("%H:%M:%S")
+    local fullTime = os.date("%Y-%m-%d") .. " " .. '星期' .. weakTab[tonumber(os.date("%w") + 1)] .. " " .. time
+    yield(Candidate("time", seg.start, seg._end, time, " 时间"))
+    yield(Candidate("time", seg.start, seg._end, fullTime, " 时间"))
+  end
 
-    if (input == "week") then
-        local weakTab = {'日', '一', '二', '三', '四', '五', '六'}
-        yield(Candidate("week", seg.start, seg._end, "周"..weakTab[tonumber(os.date("%w") + 1)], ""))
-        yield(Candidate("week", seg.start, seg._end, "星期"..weakTab[tonumber(os.date("%w") + 1)], ""))
-        yield(Candidate("week", seg.start, seg._end, "礼拜"..weakTab[tonumber(os.date("%w") + 1)], ""))
-    end
+  if (input == "date") then
+    yield(Candidate("date", seg.start, seg._end, os.date("%Y 年 %m 月 %d 日"), "日期"))
+    yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), "日期"))
+  end
+
+  if (input == "week") then
+    local week = weakTab[tonumber(os.date("%w") + 1)]
+    yield(Candidate("week", seg.start, seg._end, "周" .. week, ""))
+    yield(Candidate("week", seg.start, seg._end, "星期" .. week, ""))
+    yield(Candidate("week", seg.start, seg._end, "礼拜" .. week, ""))
+  end
 end
 
--- 将上述定义导出
 return translator
