af861e31280129768eebd4bd5fa170a22b0db7ad
cs-mac <halfmoonvic@gmail.com>
2023-01-25 07:51:09 +0800

    feat: add lunar

--- rime.lua
+++ rime.lua
@@ -2,5 +2,8 @@
 time_translator = require("time")
 number_translator = require("number")
 
+-- https://blog.csdn.net/qq_43108090/article/details/122747904
+lunar_translator = require("lunar")
+
 -- https://github.com/hchunhui/librime-lua/issues/14
 thousand_translator = require("thousand")
