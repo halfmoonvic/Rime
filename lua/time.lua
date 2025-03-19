76fb358d993734eae9fcb00e9d99cf4e11f1d6d3
cs <halfmoonvic@gmail.com>
2025-03-20 06:47:59 +0800

    chore: normailize time.lua

--- lua/time.lua
+++ lua/time.lua
@@ -31,35 +31,31 @@ local function translator(input, seg)
     return
   end
 
-  local WEEK = { 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' }
-  local WEEKCN = { '日', '一', '二', '三', '四', '五', '六' }
 
   local jq = JQNow(os.date("%Y%m%d"))
   jq = jq ~= "" and "〔" .. jq .. "〕" or ""
 
   local time = os.date("%H:%M:%S")
-  local weekEN = WEEK[tonumber(os.date("%w") + 1)]
-  local weekCN = WEEKCN[tonumber(os.date("%w") + 1)]
-  local dateEN = os.date("%Y-%m-%d")
-  local dateCN = os.date("%Y年%m月%d日")
+
+  local WEEKS = { '日', '一', '二', '三', '四', '五', '六' }
+  local week = '星期' .. WEEKS[tonumber(os.date("%w") + 1)]
+
+  local date = os.date("%Y-%m-%d")
 
   if (input == "time" or input == "shijian" or input == "sj") then
-    local fullTimeEN = dateEN .. " " .. time .. " " .. weekEN
-    local fullTimeCN = dateCN .. " " .. time .. " " .. '星期' .. weekCN
+    local fullTime = date .. " " .. time .. " " .. week
+    fullTime = jq ~= "" and fullTime .. " " .. jq or fullTime
 
     yield(Candidate("time", seg.start, seg._end, time, ""))
-    yield(Candidate("time", seg.start, seg._end, fullTimeEN, jq))
-    yield(Candidate("time", seg.start, seg._end, fullTimeCN, jq))
+    yield(Candidate("time", seg.start, seg._end, fullTime, ""))
   end
 
   if (input == "date") then
-    yield(Candidate("date", seg.start, seg._end, dateEN, jq))
-    yield(Candidate("date", seg.start, seg._end, dateCN, jq))
+    yield(Candidate("date", seg.start, seg._end, date, jq))
   end
 
   if (input == "week") then
-    yield(Candidate("week", seg.start, seg._end, weekEN, ""))
-    yield(Candidate("week", seg.start, seg._end, "星期" .. weekCN, ""))
+    yield(Candidate("week", seg.start, seg._end, week, ""))
   end
 end
 
